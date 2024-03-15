073081ed1fe70f6a25d61e5475ebffd683ae296c
Nyc27 <156249960+Nyc27@users.noreply.github.com>
2024-03-15 16:42:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const mobileToggle = document.querySelector(".mobile-nav-toggle")
+const menuVisible = document.querySelector(".mobile-nav")
+const headerColor = document.querySelector('header')
+
+mobileToggle.addEventListener('click', function () {
+
+    menuVisible.classList.toggle("visible");
+    mobileToggle.classList.toggle("rotate");
+    headerColor.classList.toggle("white-bg");
+
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+    const portataButtons = document.querySelectorAll('.portata-button');
+    const portate = document.querySelectorAll('.portate');
+  
+    portataButtons.forEach(button => {
+      button.addEventListener('click', function() {
+        event.preventDefault();
+        const portataToShow = this.getAttribute('data-portata');
+  
+        // Nascondi tutte le portate
+        portate.forEach(portate => {
+          portate.style.display = 'none';
+        });
+  
+        // Mostra solo la portata corrispondente al bottone cliccato
+        document.querySelector(`.portate.${portataToShow}`).style.display = 'flex';
+
+        document.querySelector(`#${portataToShow}`).scrollIntoView({ behavior: 'smooth' });
+      });
+    });
+  });
